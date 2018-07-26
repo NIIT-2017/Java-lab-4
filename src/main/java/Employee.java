64ab516a6edb573378e063470145fb030e71cccd
AntKuk <39840292+AntKuk@users.noreply.github.com>
2018-07-26 22:09:33 +0300

    Update Employee.java

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -54,9 +54,9 @@ interface WorkTime {
 }
 
 interface Project {
-    int project1 = 100_000;
-    int project2 = 200_000;
-    int project3 = 300_000;
+    int project1 = 100000;
+    int project2 = 200000;
+    int project3 = 300000;
 
     int ProjectPay ();
 }

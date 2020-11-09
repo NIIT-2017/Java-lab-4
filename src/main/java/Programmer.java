82e31dc2444ccc644cf464f220ec5a103a7a2144
Bear346 <57682132+Bear346@users.noreply.github.com>
2020-11-09 16:52:57 +0300

    Create Programmer.java

---
+++ src/main/java/Programmer.java
@@ -0,0 +1,5 @@
+public class Programmer extends Engineer{
+    public Programmer(int id, String name, int rate, int projectBudget, double part){
+        super(id, name, rate, projectBudget, part);
+    }
+}
